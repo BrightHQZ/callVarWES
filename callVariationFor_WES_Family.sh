5103779a9a98efbd3fc184f8bc133800b8b25b68
BrightHQZ <hqzlul@163.com>
2022-01-30 18:32:20 +0800

    Update

--- callVariationFor_WES_Family.sh
+++ callVariationFor_WES_Family.sh
@@ -58,7 +58,7 @@ fileListB=();
 samples=();
 #tools
 SOAPnuke="/data/bioTools/bin/SOAPnuke/SOAPnuke";
-GATK="/data/bioTools/bin/gatk4/gatk-package-4.2.2.0-local.jar";
+GATK="/data/bioTools/bin/gatk4/gatk-package-4.2.4.1-local.jar";
 BWA="bwa";
 SAMTOOLS="samtools";
 sambamba="/data/bioTools/bin/sambamba";
@@ -80,8 +80,8 @@ vcf38B="/data/bioTools/resource/callSNP/hg38/gatk/1000G_omni2.5.hg38.vcf";
 vcf38C="/data/bioTools/resource/callSNP/hg38/gatk/hapmap_3.3.hg38.vcf";
 
 vcf19A="/data/bioTools/resource/callSNP/hg19/gatk/gz/dbsnp_138.hg19.vcf";
-vcf19B="/data/bioTools/resource/callSNP/hg19/gatk/gz/1000G_phase1.indels.hg19.vcf";
-vcf19C="/data/bioTools/resource/callSNP/hg19/gatk/gz/Mills_and_1000G_gold_standard.indels.hg19.vcf";
+vcf19B="/data/bioTools/resource/callSNP/hg19/gatk/gz/1000G_phase1.indels.hg19.sites.vcf";
+vcf19C="/data/bioTools/resource/callSNP/hg19/gatk/gz/Mills_and_1000G_gold_standard.indels.hg19.sites.vcf";
 #perl path
 perlPath="/data/GWASCall/SH";
 #blast path
@@ -306,7 +306,7 @@ do
             #inF=$(sortBam $inF $outDir"/bam/"$sample $sample $SAMTOOLS $outDir"/log")
             #inF=$(markDup $outDir"/bam/"$sample/$inF $outDir"/bam/"$sample $sample $sambamba $outDir"/log");
             inF="$outDir/bam/$sample/$sample.sort.markdup.bam";
-            $(qualityBAM $bamdst $inF "$outDir/bam/"$sample $caputerBed);
+            #$(qualityBAM $bamdst $inF "$outDir/bam/"$sample $caputerBed);
             if [ "$refV" == "hg19" ]; then
                 inF=$(BaseRecalibrator $outDir/bam/$sample/$sample.sort.markdup.bam $outDir/bam/$sample $sample $GATK $ref $vcf19A $vcf19B $vcf19C $outDir/log/$sample)
             else
